60d6b5e9d91873a0fe73e5b8e0d0f68b39bebf86
marcelo <mleme@msn.com>
2022-05-15 03:13:51 +0000

    feat: :heavy_minus_sign: Add Unique-entity-id unit test

---
+++ src/@seedwork/domain/unique-entity-id.vo.spec.ts
@@ -0,0 +1,28 @@
+import InvalidUuidErro from "../erros/invalid-uuid.error"
+import UniqueEntityId from "./unique-entity-id.vo"
+import {validate as validateUUID } from "uuid"
+
+
+describe('UniqueEntityID unit test', () => {
+
+    const validateSpy = jest.spyOn(UniqueEntityId.prototype as any, 'validate')
+
+    it('Should trow error when uuid is invalid', () => {
+       expect( () => new UniqueEntityId("Fake id")).toThrow(new InvalidUuidErro())
+       expect(validateSpy).toBeCalled()
+    })
+
+    it("Should accept a uuid passed on contructor", () => {
+        const uuid = "d1d3c3e4-5dfe-4e0b-a946-09f6ea8fcb0f"
+        const vo = new UniqueEntityId(uuid)
+        expect(vo.id).toBe(uuid)
+        expect(validateSpy).toBeCalled()
+    })
+
+    it("Should accept a uuid passed on contructor", () => {
+        const vo = new UniqueEntityId()
+        expect(validateUUID(vo.id)).toBeTruthy()
+        expect(validateSpy).toBeCalled()
+    })
+
+})
